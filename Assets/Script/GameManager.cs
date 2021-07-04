7eead25a4d023224eeb9b59a861fd038fe2baa12
ksan2525 <kotetu824@gmail.com>
2021-07-04 15:42:40 +0900

    ターンエンドを実装した

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] CardController cardPrefab;
-    [SerializeField] Transform playerHand,playerField;
+    [SerializeField] Transform playerHand, playerField, enemyField;
+
+    bool isPlayerTurn = true;
+    List<int> deck = new List<int>() { 1, 2, 3, 1, 1, 2, 2, 3, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3 };
 
     private void Start()
     {
         StartGame();
     }
 
-    void StartGame()
+    void StartGame()//初期値の設定
     {
-        
-            CreateCard(1, playerHand);
-            CreateCard(2, playerHand);
-            CreateCard(2, playerHand);
-            CreateCard(1, playerHand);
-            CreateCard(2, playerHand);
-            CreateCard(1, playerHand);
-        
+        //初期手札を配る
+        SetStartHand();
+        //ターンの決定
+        TurnCalc();
+
 
     }
 
@@ -29,5 +29,73 @@ public class GameManager : MonoBehaviour
     {
         CardController card = Instantiate(cardPrefab, place);
         card.Init(cardID);
+    }
+
+    void DrowCard(Transform hand)//カードを引く
+    {
+        //デッキがないなら引かない
+        if (deck.Count == 0)
+        {
+            return;
+        }
+
+        CardController[] playerHandCardList = playerHand.GetComponentsInChildren<CardController>();
+
+        if(playerHandCardList.Length < 6)
+        {
+            //デッキの一番上のカードを切り取り、手札に加える
+            int cardID = deck[0];
+            deck.RemoveAt(0);
+            CreateCard(cardID, hand);
+        }
+        
+    }
+
+    void SetStartHand()//手h打を三枚配る
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            DrowCard(playerHand);
+        }
+    }
+
+    void TurnCalc()//ターンを管理する
+    {
+        if (isPlayerTurn)
+        {
+            PlayerTurn();
+        }
+        else
+        {
+            EnemyTurn();
+        }
+    }
+
+    public void ChangeTurn()//ターンエンドボタンにつける処理
+    {
+        isPlayerTurn = !isPlayerTurn;//ターンを逆にする
+        TurnCalc();//ターンを相手に回す
+    }
+
+    void PlayerTurn()
+    {
+        Debug.Log("Playerのターン");
+
+        DrowCard(playerHand);//手札を一枚加える
+    }
+
+    void EnemyTurn()
+    {
+        Debug.Log("Enemyのターン");
+
+        CardController[] enemyFieldCardList = enemyField.GetComponentsInChildren<CardController>();
+
+        if(enemyFieldCardList.Length < 5)
+        {
+            CreateCard(1, enemyField);//カードを召喚
+
+            
+        }
+        ChangeTurn();//ターンエンドする
     }
 }
